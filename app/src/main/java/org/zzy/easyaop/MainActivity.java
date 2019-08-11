dcd759d7599e363d12f5aa95b10363c75eca1795
zhouzhengyi <zhouzhengyi@01zhuanche.com>
2019-08-11 15:16:06 +0800

    1.添加自动tryCatchAOP 2.分为按包名和按注解

--- app/src/main/java/org/zzy/easyaop/MainActivity.java
+++ app/src/main/java/org/zzy/easyaop/MainActivity.java
@@ -2,6 +2,11 @@ package org.zzy.easyaop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.zzy.aoplib.exception.annotation.AutoTryCatch;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void safe(View view){
+        String str=null;
+        str.indexOf("a");
+    }
+
+
 }
